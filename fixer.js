b22386890bd15387192ec2e904f146051a0ac1be
Dawid Skowron <dawidd14@gmail.com>
2018-06-05 23:17:46 +0200

    initial

---
+++ fixer.js
@@ -0,0 +1,5 @@
+/* The new version of mBank transaction panel is using a lifting class in html and body tag. */
+/* So we just remove them. */
+
+document.documentElement.classList.remove('lifting');
+document.body.classList.remove('lifting');
